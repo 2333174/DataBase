13209c9d42b5835c0625132487e3ef9467fc4e7a
2333174 <1741603127@qq.com>
2019-03-02 14:33:09 +0800

    登录

--- yjy/Login/PassWordWindow.xaml.cs
+++ yjy/Login/PassWordWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySql.Data.MySqlClient;
 
 namespace Login
 {
@@ -44,20 +45,50 @@ namespace Login
             {
                 MessageBox.Show("请输入密码！");
             }
-            //判断账号密码是否正确
-            if (user.Text.ToString() == "yjy" && password.Password.ToString() == "00000")
+            //判断账号密码是否正确try
+            try 
             {
-                //正确，登陆成功
-                if ("manage" == select)
+                string sqlstr = "server = 192.168.154.70; uid = root; pwd = root; database = dbtest";
+                MySqlConnection conn = new MySqlConnection(sqlstr);
+                conn.Open();
+                string UserNamestr = string.Format("SELECT * FROM Login where User = '{0}' and Password = {1};",
+                    user.Text, password.Password);
+                MySqlCommand comm = new MySqlCommand(UserNamestr, conn);
+                MySqlDataReader dr = comm.ExecuteReader();
+                if (dr.Read())
                 {
-                    ManageSystem manageWindow = new ManageSystem();
-                    manageWindow.ShowDialog();
-                    this.Hide();
+                    string name = dr.GetString(dr.GetOrdinal("User"));
+                    string pwd = dr.GetString(dr.GetOrdinal("Password"));
+                    if (name == user.Text && pwd == password.Password)
+                    {
+                        MessageBox.Show("登录成功");
+                        return;
+                    }
                 }
-                
+                else
+                {
+                    MessageBox.Show("用户名或密码错误，请检查后输入！", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+                }
             }
-            else
-                MessageBox.Show("账号或密码有误！");
+            catch (Exception ex)
+            {
+
+                MessageBox.Show(ex.Message.ToString());
+            }
+
+        //    if (user.Text.ToString() == "yjy" && password.Password.ToString() == "00000")
+        //    {
+        //        //正确，登陆成功
+        //        if ("manage" == select)
+        //        {
+        //            ManageSystem manageWindow = new ManageSystem();
+        //            manageWindow.ShowDialog();
+        //            this.Hide();
+        //        }
+                
+        //    }
+        //    else
+        //        MessageBox.Show("账号或密码有误！");
         }
     }
 }
