6ab818516bb68798aa696bdf7fb595fcbf780c12
pink1999 <969909834@qq.com>
2019-03-16 15:26:08 +0800

    登陆

--- yjy/Login/PassWordWindow.xaml.cs
+++ yjy/Login/PassWordWindow.xaml.cs
@@ -48,11 +48,12 @@ namespace Login
             {
                 MessageBox.Show("请输入密码！");
             }
-            
+
             //数据库判断账号密码是否正确try
             try
             {
-                string sqlstr = "server = 10.132.89.16; uid = root; pwd = root; database = gymdb";
+                string sqlstr = "server = 192.168.154.76; uid = root; pwd = root; database = gymdb";
+                //连接数据库
                 MySqlConnection conn = new MySqlConnection(sqlstr);
                 conn.Open();
                 string UserNamestr = string.Format("SELECT * FROM Login where UName = '{0}' and Password = '{1}'and Role='{2}' ;",
@@ -65,44 +66,63 @@ namespace Login
                     string name = dr.GetString(dr.GetOrdinal("UName"));
                     string pwd = dr.GetString(dr.GetOrdinal("Password"));
                     string role = dr.GetString(dr.GetOrdinal("Role"));
-                    if (name.Equals(user.Text) && pwd.Equals(password.Password) && role.Equals(select.ToString()))
+                    //如果身份和系统相符合
+                    if (role.Equals(select.ToString()))
                     {
-                        //MessageBox.Show("登录成功");
-                        if (1 == select)
+                        //如果账号密码正确 打开相应系统
+                        if (name.Equals(user.Text) && pwd.Equals(password.Password))
                         {
-                            this.Close();
-                            ManageSystem manageWindow = new ManageSystem();
-                            manageWindow.ShowDialog();
-                        }
-                        if (2 == select)
-                        {
-                            string weight = dr.GetString(dr.GetOrdinal("Weight"));//weight为0是裁判，为1是主裁判
-                            if (weight == "0")
+                            //MessageBox.Show("登录成功");
+                            if (1 == select)
                             {
+                                //new一个管理系统 并显示
+                                ManageSystem manageWindow = new ManageSystem();
                                 this.Close();
-                                GradeSystem gradeSystem = new GradeSystem();
-                                gradeSystem.ShowDialog();
+                                manageWindow.ShowDialog();
+                               
                             }
-                            else
+                            if (2 == select)
                             {
-                                this.Close();
-                                GSForMajorJudge gSForMajorJudge = new GSForMajorJudge();
-                                gSForMajorJudge.ShowDialog();
+                                string weight = dr.GetString(dr.GetOrdinal("Weight"));//weight为0是裁判，为1是主裁判
+                                if (weight == "0")
+                                {
+                                    
+                                    GradeSystem gradeSystem = new GradeSystem();
+                                    this.Close();
+                                    gradeSystem.ShowDialog();
+                                    
+                                }
+                                else
+                                {
+                                    
+                                    GSForMajorJudge gSForMajorJudge = new GSForMajorJudge();
+                                    this.Close();
+                                    gSForMajorJudge.ShowDialog();
+                                    
+                                }
                             }
+                            if (0 == select)
+                            {
+                                //打开打分系统并关闭本窗口
+                                
+                                Window1 signup = new Window1();
+                                this.Close();
+                                signup.ShowDialog();
+                                
+                            }
+                            return;
                         }
-                        if (0 == select)
+                        else
                         {
-                            //打开打分系统并关闭本窗口
-                            this.Close();
-                            Window1 signup = new Window1();
-                            signup.ShowDialog();
+                            MessageBox.Show("用户名或密码错误，请检查后输入！", "", MessageBoxButton.OK, MessageBoxImage.Warning);
                         }
-                        return;
                     }
-                }
-                else
-                {
-                    MessageBox.Show("用户名或密码错误，请检查后输入！", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    //如果身份与系统不相符就提示用户重新选择
+                    else
+                    {
+                        MessageBox.Show("您的身份与该系统不匹配！请重新选择！", "", MessageBoxButton.OK, MessageBoxImage.Warning);
+                    }
+
                 }
             }
             catch (Exception ex)
