ccdc7202810047e05fabdbd7e2b50079b8bc6bf0
my0sotis <1274034280@qq.com>
2019-03-26 00:20:21 +0800

    UI v1.0.0
    实现在同一窗口切换界面，准备实现切换动画功能。

---
+++ zpc/Login/LoginPage.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// LoginPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class LoginPage : Page
+    {
+        public int select { get; set; }
+        public LoginPage(int s)
+        {
+            InitializeComponent();
+            select = s;
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            ChangePage.Content = new Frame()
+            { Content = new ChosePage() };
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ zpc/Login/MainWindow.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Login
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ChangePage.Content = new Frame()
+            { Content = new ChosePage()};
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+            Application.Current.Shutdown();
+        }
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    //用参数select来传递数据,0报名,1管理,2打分
+        //    int select;
+        //    //判断用户选择的是哪个窗口并显示该窗口
+        //    if (radio_sign.IsChecked==true)
+        //    {
+        //        select = 0;
+        //        PassWordWindow passWordWindow = new PassWordWindow(select);
+        //        //关闭当前窗口
+        //        ChangePage.Content = new Frame()
+        //        {
+        //            Content = new LoginPage()
+        //        };
+        //    }
+        //    if(radio_manage.IsChecked==true)
+        //    {   
+        //        select = 1;
+        //        //打开管理系统窗口
+        //        PassWordWindow passWordWindow = new PassWordWindow(select);
+        //        //关闭当前窗口
+        //        //passWordWindow.ShowDialog();
+        //        ChangePage.Content = new Frame()
+        //        {
+        //            Content = new LoginPage()
+        //        };
+        //    }
+        //    if(radio_grade.IsChecked==true)
+        //    {
+        //        select = 2;
+        //        PassWordWindow passWordWindow = new PassWordWindow(select);
+        //        //关闭当前窗口
+        //        ChangePage.Content = new Frame()
+        //        {
+        //            Content = new LoginPage()
+        //        };
+        //        //passWordWindow.ShowDialog();
+
+        //    }
+        //}
+    }
+}
