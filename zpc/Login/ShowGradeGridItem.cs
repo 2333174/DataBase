9fe685a173d1e387bdd82c44665f10fc30970764
AsteriaChiang <AsteriaChiang@outlook.com>
2019-04-27 15:08:56 +0800

    显示成绩排名界面

---
+++ zpc/Login/ShowGradeGridItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Login
+{
+    class ShowGradeGridItem
+    {
+        //姓名 号码 年龄 分数
+        public float rank { get; set; }
+        public string name { get; set; }
+        public string atheleteID { get; set; }
+        public string groupname { get; set; }
+        public float atheletegrade { get; set; }
+        public string game { get; set; }
+        public string type { get; set; }
+        public ShowGradeGridItem(float rank,string name, string atheleteID, string groupname, float atheletegrade, string game, string type)
+        {
+            this.rank = rank;
+            this.name = name;
+            this.atheleteID = atheleteID;
+            this.groupname = groupname;
+            this.atheletegrade = atheletegrade;
+            this.game = game;
+            this.type = type;
+        }
+    }
+}
